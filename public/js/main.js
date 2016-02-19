405529e872ace946da028e86862d55cd11f0bced
Jess Lee <jessleenyc@gmail.com>
2016-02-19 13:01:42 -0500

    adds viewport tag

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,13 @@ $( document ).ready(function() {
   // Initialize collapse button
   $(".button-collapse").sideNav();
 
+  fluidvids.init({
+    selector: ['iframe', 'object'], // runs querySelectorAll()
+    players: ['player.vimeo.com'] // players to support
+  });
+
+  fluidvids.render();
+
   //change text
   var text = [
     { name: "FILMS.",
@@ -54,18 +61,6 @@ $( document ).ready(function() {
   
 
   var popOutFunc = function() {
-
-    // $popOver.animate(// invoking .animate method
-    //   {left: '77%'}, // Go to 65% from the left
-    //   500, // animation duration of a half seond
-    //   function(){ console.log('done')}// Tell me I'm done
-    //   )
-
-    // $('body').on('click', function() {
-    //   $popOver.css({position: 'fixed', bottom: '0', left: '100%'});
-    // });
-
-
       $popOver.toggle( function() {
         $('body').on('click', function() {
         console.log('clicked body');
@@ -78,5 +73,7 @@ $( document ).ready(function() {
   };
 
   $('#snapmenu').on('click', popOutFunc);
+
+
 
 });
